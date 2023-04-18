808a5a9ba9ec4fcfb5215f78ec2b5e7cadbde965
Matvey <90297430+jewomskiy@users.noreply.github.com>
2023-04-18 17:37:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from MyLink import MyLink
 from TeleBot import BotStax
 
-link = MyLink('localhost', 'root', '', 'a354387_stax')
-bot = BotStax('1682040196:AAEoOMe3S8e4YDrsXj3uQRLvTubiu7vxZVQ', link)
+link = MyLink('localhost', 'root', '', 'blabla')
+bot = BotStax('key', link)
